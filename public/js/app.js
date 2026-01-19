7fc38a88880a866aba13c9a538c48f527dffd1a4
ayoub <ab.naciri.official@gmail.com>
2026-01-19 13:04:51 +0100

    feat: hero for mobile

--- public/js/app.js
+++ public/js/app.js
@@ -2,11 +2,13 @@ const modal = document.querySelector(".modal")
 const btn = document.querySelector(".modalTriger1")
 const h1 = document.querySelector("h1")
 
-btn.onclick =  () =>{
-    modal.style.top = "-0vh"
+btn.addEventListener( "click" , (e)=> {
+e.preventDefault()
+    modal.style.top = "0vh"
     modal.style.display = "flex"
     // modal.style.transform = "all 0.4s ease" 
-}
+})
+
 
 modal.onclick = () =>
 {

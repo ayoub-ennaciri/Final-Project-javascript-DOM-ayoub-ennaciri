20dcb310ff237ac59a08eabe889cd9ffdefcc083
ayoub <ab.naciri.official@gmail.com>
2026-01-19 13:48:48 +0100

    feat: added a second modal

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,6 @@
 const modal = document.querySelector(".modal")
 const btn = document.querySelector(".modalTriger1")
+const btn2 = document.querySelector(".modalTriger2")
 const h1 = document.querySelector("h1")
 
 btn.addEventListener( "click" , (e)=> {
@@ -9,6 +10,12 @@ e.preventDefault()
     // modal.style.transform = "all 0.4s ease" 
 })
 
+btn2.addEventListener( "click" , (e)=> {
+e.preventDefault()
+    modal.style.top = "0vh"
+    modal.style.display = "flex"
+    // modal.style.transform = "all 0.4s ease" 
+})
 
 modal.onclick = () =>
 {

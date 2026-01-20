a022e45064291c020735fdbaa044c980749ecb12
ayoub <ab.naciri.official@gmail.com>
2026-01-20 04:07:23 +0100

    feat: added hero video modal

--- public/js/app.js
+++ public/js/app.js
@@ -5,6 +5,7 @@ let X = document.querySelector(".X")
 
 
 burgerIcon.addEventListener("click", (e) => {
+    e.preventDefault()
     console.log("hi")
     sideContenair.style.display = "flex"
     sideContenair.style.width = "100vw"
@@ -13,12 +14,12 @@ burgerIcon.addEventListener("click", (e) => {
     sideContenair.style.left = "0"
     sideContenair.style.padding ="15vh 2vw"
     sideContenair.style.right = "2vw"
-    sideContenair.style.position = "absolute"
+    sideContenair.style.position = "fixed"
     sideContenair.style.backgroundColor = "#26242ba2";
     sideMenu.style.gap = "20px"
     sideMenu.style.borderRadius = "5px"
     sideMenu.style.padding = "20px 20px "
-    sideMenu.style.display = "flex"
+    sideMenu.style.display = "flex" 
     sideMenu.style.flexDirection = "column" ;
     sideMenu.style.width    ="100%"
     sideMenu.style.height = "100%"
@@ -31,4 +32,20 @@ burgerIcon.addEventListener("click", (e) => {
 X.addEventListener("click", (e) =>
 {
     sideContenair.style.display = "none"
+    document.body.classList.add("overflow-hidden");
+
+})
+
+// *hero vedio 
+let watch = document.querySelector(".watch")
+let videoModal = document.querySelector(".videoModal")
+
+watch.addEventListener("click", ()=>
+{
+    videoModal.style.display = "flex";
+})
+
+videoModal.addEventListener("click", ()=>
+{
+    videoModal.style.display = "none";
 })

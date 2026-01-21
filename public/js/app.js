3b450f0b299772933f0d7d9897ed2a85245deaaf
ayoub <ab.naciri.official@gmail.com>
2026-01-21 03:18:06 +0100

    feat: added chefs section

--- public/js/app.js
+++ public/js/app.js
@@ -124,7 +124,155 @@ option4.addEventListener("click", (e)=>
 })
 
 
+// *testimonials
 
+const caroussel = document.querySelectorAll(".caroussel");
+let track = document.querySelectorAll(".track")
+let next = document.querySelectorAll(".next")
+let prev = document.querySelectorAll(".prev")
+let doted = document.querySelectorAll(".doted")
+
+// appending and prepending a copy of the first and last image
+
+const init = ()=>
+{
+  // make sure that the same number of caroussels matches the number of tracks
+  // if false throw a syntax error 
+    if(caroussel.length != track.length)
+    throw new SyntaxError("the number of carousels and their tracks does not match")
+
+  // go through each caroussel and get there tracks and append a copy of the fisrt slide
+  // and prepend a copy of the last slide and translate the track to the index 1 slide 
+  // aka the first original slide
+    let i = 0;
+    while(caroussel[i])
+    {
+        let track = caroussel[i].querySelector(".track");
+        let slide = track.querySelectorAll(".slide");
+          // this is an index of the current slide wich will be used later by other functions
+        caroussel[i].cont = 1 
+        if(slide.length > 0)
+        {
+            track.append(slide[0].cloneNode(true));
+            track.prepend(slide[slide.length -1].cloneNode(true));
+            track.style.transform = `translateX(-${slide[0].clientWidth}px)`
+
+            // if carousel has doted class , add radio buttons to the carousel
+            if(caroussel[i].classList.contains("doted"))
+            {
+                let j = 0
+                while(j < slide.length)
+                {
+                    let radio = document.createElement("input")
+                    radio.setAttribute("type","radio")
+                    radio.setAttribute("data-radio",`car-radio${i}`)
+                    
+                    radio.setAttribute("name",`radio${i}`)
+                    caroussel[i].append(radio)
+                    j++;
+                }
+            }
+        }
+        i++;
+    } 
+}
+
+// function for advancing forward to the next slide
+const forward = (e) =>
+{
+  // each button should have a "data-caro" attribute wich should contain the id of the target caroussel
+if(e.currentTarget.hasAttribute("data-caro"))
+  {
+    // store the caroussel that we got bassed on the id
+    let targetCarousel = document.querySelector(`#${e.currentTarget.getAttribute("data-caro")}`)
+    // get the track of that caroussel
+    let targetTrack = targetCarousel.children
+    // if the current slide conter is below or equal to our slides number advance to the next slide
+    if(targetCarousel.cont <= targetTrack[0].children.length-2) 
+    {
+      // this checks if have 
+      if(targetTrack[0])
+      {    
+        targetCarousel.cont++;
+        targetTrack[0].style.transition = "transform 0.4s ease"
+        targetTrack[0].style.transform = `translateX(-${targetCarousel.cont * targetTrack[0].children[0].clientWidth}px)`
+      }
+    }
+        console.log(targetCarousel.cont);
+    targetTrack[0].addEventListener("transitionend" ,() =>
+        {
+          if(targetCarousel.cont >   targetTrack[0].children.length-2)
+          {
+            console.log("hhi");
+            targetCarousel.cont = 1
+            targetTrack[0].style.transition = "none"
+            console.log(targetTrack[0].children[0].clientWidth)
+            targetTrack[0].style.transform = `translateX(-${targetTrack[0].children[0].clientWidth}px)`
+            return
+          }
+        })
+  }
+}
+
+const previous = (e) =>
+{
+  console.log("prev");
+  
+  console.log(e.currentTarget.getAttribute("data-caro"));
+  
+  if(e.currentTarget.hasAttribute("data-caro"))
+  {
+
+    let targetCarousel = document.querySelector(`#${e.currentTarget.getAttribute("data-caro")}`)
+    
+    let targetTrack = targetCarousel.children
+    console.log(targetTrack[0].children.length);
+
+    if(targetCarousel.cont > 0) 
+    {
+      if(targetTrack[0] && targetTrack[0].children[0])
+        {
+          
+          targetCarousel.cont--;
+          targetTrack[0].style.transition = "transform 0.4s ease"
+          targetTrack[0].style.transform = `translateX(${-targetCarousel.cont * targetTrack[0].children[0].clientWidth}px)`
+        }
+      }
+      console.log(targetCarousel.cont);
+    
+
+    targetTrack[0].addEventListener("transitionend" ,() =>
+        {
+          if(targetCarousel.cont <= 0)
+        { 
+          targetCarousel.cont = targetTrack[0].children.length-2
+          targetTrack[0].style.transition = "none"
+          targetTrack[0].style.transform = `translateX(-${(targetTrack[0].children.length-2) * targetTrack[0].children[0].clientWidth}px)`
+          return
+        }
+        })
+
+  }
+}
+
+init()
+
+
+i = 0
+
+while(next[i])
+{
+  next[i].addEventListener("click", forward)
+  i++
+}
+
+i= 0
+
+while(prev[i])
+{
+  prev[i].addEventListener("click", previous)
+  i++
+}
 
 
 
